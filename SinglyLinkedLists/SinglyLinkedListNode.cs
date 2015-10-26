f0a07dd48b3b04a6eb8882f7ea28df8a254d79fe
Sally Fisher <sqfisher@gmail.com>
2015-10-26 15:49:56 -0500

    Milestone 1: passing NodeConstructorStoresName, NodeDefaultNext, NodeSetNext, NodeNextCantBeThis, NodeToString

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -15,14 +15,22 @@ namespace SinglyLinkedLists
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return next; }
+            set {
+                    if (this == value)
+                    {
+                        throw new ArgumentException();
+                    } else
+                    {
+                        next = value;
+                    }
+                }
         }
 
         private string value;
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return value; }
         }
 
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
@@ -39,10 +47,14 @@ namespace SinglyLinkedLists
 
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
-
+            this.value = value;
             // Used by the visualizer:
             allNodes.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return this.value;
         }
 
         // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
